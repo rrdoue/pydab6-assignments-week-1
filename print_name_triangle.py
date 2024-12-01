e18430cf0304ca458b0e203b80daa51141bc92fa
Ray Doue <rrdoue@gmail.com>
2024-12-01 09:47:14 -0600

    I changed back to the enumerate function(?) and also added in some debug statements that helped me understand a little more of what was going on.  Reuven will show us how to retrieve an earlier version if we need it, but this one is functional using debug None or 1.

--- print_name_triangle.py
+++ print_name_triangle.py
@@ -1,20 +1,29 @@
 #! /usr/bin/env python3
 
-# Section 4, Exercise 4b: Name Triangle 
+# Section 4, Exercise 4: Name Triangle
+# Ask the user to enter their name.  Print their name as a triangle.  
+#   For example, the first letter, another line withtwo letters, another line 
+#   with three letters, until you've printed their full name.  This has been 
+#   called a name triangle.
 
-# Ask the user to enter their name. 
-# Print their name as a triangle. 
-# For example, the first letter on one line, the next line with two letters,  
-#   the next line with three letters, until you've printed their entire name. 
-# This has been called a name triangle.
+debug = None 
+s = ' '
 
 user_name = input('Please enter your name: ').strip()
 
-# Reuven used a range(len(user_name)) instead of an enumerate. 
-# He also left off the initial 0, just using [:index+1]
-# for index, one_letter in enumerate(user_name):
-#     print(user_name[:index+1])
+if debug:
+    print(f'\nName Length: {len(user_name)} character(s)\n')
 
-for idx in range(len(user_name)):
-    print(user_name[:idx+1])
+# My original version, with Reuven's improvements, which I like better
+for idx, one_letter in enumerate(user_name, start=1):
+
+    if debug:
+        print(f'{user_name[:idx]}{s * (len(user_name) + 2 - idx)}{idx-1}')
+    else:
+        print(f'{user_name[:idx]}')
+
+# Reuven used a range(len(user_name)) instead of an enumerate and also left off 
+#   the initial 0, just using [:idx+1]
+# for idx in range(len(user_name)):
+#     print(user_name[:idx+1])
 
