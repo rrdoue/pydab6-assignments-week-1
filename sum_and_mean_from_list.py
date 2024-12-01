ee2868e79218a6b60f4cffda57a555a60ae9eafb
Ray Doue <rrdoue@gmail.com>
2024-12-01 08:32:41 -0600

    Initial commit for the sum and average of requesting numbers from a user, Section 5, Exercise 1.  Norman Eliaser had some good additions, but we can update the file anytime.

---
+++ sum_and_mean_from_list.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+# Section 5, Exercise 1, Sum and Average Numbers from a User
+# Request numbers from a user, then when they stop by entering an empty string, 
+#   find the sum and average of the numbers. 
+
+debug = None
+
+total = 0
+average = 0
+number_list = []
+
+while True:
+    value = input('Please enter a number (or leave blank to end input): ').strip()
+    if value:
+        if value.isnumeric():
+            number_list.append(int(value))
+        else:
+            print('The value you provided is not a number, please try again, or leave the input blank to end input.')
+            continue
+    else:
+        break
+
+total = sum(number_list)
+
+average = round(total / len(number_list), 2)
+
+print(f'\nThe sum of the numbers is {total}.')
+print(f'The average of the numbers is {average}.')
+
