5b8070ba14e9dd4b6c8c8d9408d7b300fcbaae38
Ray Doue <rrdoue@gmail.com>
2024-12-06 14:04:35 -0600

    This turned out to have a significant number of changes, nothing very substantial, but perhaps helpful although involved lots of time.  General changes follow: 1. Moved around some of the commented suggestions that Reuven had in his approve d solution. 2. Renamed some of the variable names following Reuven and Faan's suggestions for clarity. 3. Added some json formatting to the debug statement to learn a little about what appears to be a common format. I commented out the sort in order to retain input order when printing the dictionary in the debug statement. 4. Used the variable daily rainfall to set a numeric value, then use that value thereafter in order to avoid executing multiple int() statements later. 5. Changed the processing in the event of a non-numeric daily rainfall value to print a statement, then go back to retrying at entering a city.  I encountered multiple occurrences where I lost track of where I was, entering a city name when I had not yet submitted a daily rainfall value.

--- cities_and_rainfall.py
+++ cities_and_rainfall.py
@@ -8,30 +8,54 @@
 #   a city, that is, for more than one day, so add the daily rainfall to the 
 #   existing city each time (or start a new city as needed).  At the end, print 
 #   all of the city and rainfall values.
+#   Note leaving out the sorting in the json formatted dictionary preserves entry
+#   order, which may be helpful for debugging or understanding processing.
+
+# Faan DeSwardt suggested using some json based on what he sees in his work
+import json
 
 debug = None
 
-rain = {}
-city = ''
-total_rainfall = 0 # in mm hah
+total_rainfall = {}
+city_name = ''
+daily_rainfall = 0  # in mm hah, this is kind of a hint variable for what I called daily amount
 
 while True:
-    city = input('Please enter the name of a city: ').strip().title()
+    city_name = input('Please enter the name of a city: ').strip().title()
 
-    if not city:
+    if not city_name:
         break
 
-    amount = input('Please enter the rainfall for a day (whole mm): ').strip()
+    daily_amount = input('Please enter the rainfall for a day (whole mm): ').strip()
 
-    if not amount.isnumeric():
-        amount = input('Rainfall amount is not a whole number, please try again: ')
-
-    if not rain.get(city):
-        rain[city] = int(amount)
+    if not daily_amount.isnumeric():
+        print('We\'re encountering a problem with the numeric value, retrying with city name.\n')
+        continue
     else:
-        rain[city] += int(amount)
+        daily_rainfall = int(daily_amount)
+
+    # Original implementation prior to checking approved solution
+    #if not total_rainfall.get(city_name):
+    #    total_rainfall[city_name] = int(daily_amount)
+    #else:
+    #    total_rainfall[city_name] += int(daily_amount)
+
+    # Reuven demonstrated a previous way that works
+    # if city_name in total_rainfall:
+    #     total_rainfall[city_name] += daily_amount
+    # else:
+    #     total_rainfall[city_name] = daily_amount
+
+    # Reuven had a snazzier way to do this as shown below or using <dictionary>.setdefault()
+    # total_rainfall[city_name] = total_rainfall[city_name].setdefault(0)
+    # total_rainfall[city_name] = total_rainfall[city_name] += daily_amount
+
+    # Reuven's version using <dictionary>.get()
+    total_rainfall[city_name] = total_rainfall.get(city_name, 0) + daily_rainfall
+
+if debug:
+    print(f'\nCurrent dictionary status:\n{json.dumps(total_rainfall, indent=2)}') # sort_keys=True,
 
 print(f'\nRainfall totals for each city follow:')
-for key, value in sorted(rain.items()):
+for key, value in sorted(total_rainfall.items()):
     print(f'{key}: {value} mm')
-
